3851228d954477e2b30b876300de54e25496dd67
Horea Oros <horea.oros@gmail.com>
2025-02-25 10:42:37 +0200

    add first project

---
+++ 01 - 25 feb/01 - 25 feb/Program.cs
@@ -0,0 +1,4 @@
+﻿World w = new World();
+w.SayHello();
+
+World w2 = new World("W2");
---
+++ 01 - 25 feb/01 - 25 feb/World.cs
@@ -0,0 +1,13 @@
+﻿public class World
+{
+    // metoda constructor implicit
+    public World()
+    {
+        Console.WriteLine("World created");
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}

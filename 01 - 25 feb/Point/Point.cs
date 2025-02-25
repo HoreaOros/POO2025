07efa2fecc8b47af2c4f3882f3c634b2ec712d64
Horea Oros <horea.oros@gmail.com>
2025-02-25 12:38:55 +0200

    add Point project

---
+++ 01 - 25 feb/Point/Point.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Point
+{
+    private float x;
+    private float y;
+
+    public Point(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public override string ToString()
+    {
+        return $"({x}, {y})";
+    }
+
+
+    public float ManhattanDistanceTo(Point p2)
+    {
+        return Math.Abs(x - p2.x) + Math.Abs(y - p2.y);
+    }
+
+    public float EuclideanDistanceTo(Point p2)
+    {
+        return (float)Math.Sqrt(Math.Pow(x - p2.x, 2) +  Math.Pow(y - p2.y, 2));   
+    }
+}
---
+++ 01 - 25 feb/Point/Program.cs
@@ -0,0 +1,8 @@
+﻿Point p = new Point(3, 4);
+Console.WriteLine(p);
+
+
+Point p2 = new Point(1, 5);
+
+Console.WriteLine($"Distanta euclidiana dintre {p} si {p2} este {p.EuclideanDistanceTo(p2)}");
+Console.WriteLine($"Distanta Manhattan dintre {p} si {p2} este {p.ManhattanDistanceTo(p2)}");

e6554dd46c41f79a4ae5f3d24b63a3a8c64689d8
Horea Oros <horea.oros@gmail.com>
2025-03-04 11:03:33 +0200

    add Complex class

---
+++ 02 - 4 mar/02 - 4 mar/Complex.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+internal class Complex
+{
+    #region Fields
+    private double re;
+    private double im;
+    #endregion
+
+    #region Constructors
+    //public Complex() : this(0, 0)
+    //{
+
+    //}
+    public Complex(double re = 0, double im = 0)
+    {
+        this.re = re;
+        this.im = im;
+    }
+    #endregion
+
+    #region Methods
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append('(');
+        sb.Append(re);
+        if (im < 0)
+            sb.Append('-');
+        else
+            sb.Append('+');
+        sb.Append(Math.Abs(im));
+        sb.Append('i');
+        sb.Append(')');
+
+        return sb.ToString();
+    }
+
+    public Complex Add(Complex c2)
+    {
+        return new Complex(re + c2.re, im + c2.im);
+    }
+    public Complex Subtract(Complex c2)
+    {
+        return new Complex(re - c2.re, im - c2.im);
+    }
+    public Complex Multiply(Complex c2)
+    {
+        Complex m = new Complex();
+        m.re = re * c2.re - im * c2.im;
+        m.im = re * c2.im + im * c2.re;
+        return m;
+
+        //return new Complex(re * c2.re - im * c2.im, re * c2.im + im * c2.re);
+    }
+    #endregion
+
+    #region Properties
+    public double Modul
+    {
+        get
+        {
+            return Math.Abs(Math.Pow(re, 2) + Math.Pow(im, 2));
+        }
+    }
+    #endregion
+}
---
+++ 02 - 4 mar/02 - 4 mar/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Complex c1 = new Complex(3, 4); // 3 + 4i
+Complex c2 = new Complex(2, -5); // 2 - 5i
+Console.WriteLine(c1);
+Console.WriteLine(c2);
+
+Complex c3 = c1.Add(c2);
+Console.WriteLine(c3);
+
+Complex c4 = c1.Multiply(c2);
+Console.WriteLine(c4);
+
+Console.WriteLine($"Modulul lui {c1} este {c1.Modul}");

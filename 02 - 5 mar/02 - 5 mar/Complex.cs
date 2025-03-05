f88c88129511968d1be2f83fa2cefbf1b96b4f60
Horea Oros <horea.oros@gmail.com>
2025-03-05 13:52:24 +0200

    add funcionality to complex class

--- 02 - 5 mar/02 - 5 mar/Complex.cs
+++ 02 - 5 mar/02 - 5 mar/Complex.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text;
 
-internal class Complex
+internal class Complex: IEquatable<Complex>
 {
     #region Fields
     private double re;
@@ -31,8 +31,11 @@ internal class Complex
         sb.Append(")");
         return sb.ToString();
     }
-
-    public Complex Add(Complex c2)
+    public override bool Equals(object? obj)
+    {
+        return this.Equals(obj as Complex);
+    }
+    private Complex Add(Complex c2)
     {
         Complex c = new Complex();
         c.re = this.re + c2.re;
@@ -40,8 +43,70 @@ internal class Complex
 
         return c;
     }
+
+    public Complex Multiply(Complex c2)
+    {
+        Complex r = new Complex();
+
+        r.re = this.re * c2.re - this.im * c2.im;
+        r.im = this.re * c2.im + this.im * c2.re;
+
+        return r;
+    }
+
+    public bool Equals(Complex? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+        // Optimization for a common success case.
+        if (Object.ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        // If run-time types are not exactly the same, return false.
+        if (this.GetType() != other.GetType())
+        {
+            return false;
+        }
+
+        return this.re == other.re && this.im == other.im;
+    }
+    public override int GetHashCode()
+    {
+        return (re, im).GetHashCode();
+    }
     #endregion
 
     #region Properties
     #endregion
+
+    #region Operator overloads
+    public static Complex operator +(Complex a, Complex b)
+    {
+        return a.Add(b);
+    }
+    public static bool operator ==(Complex lhs, Complex rhs)
+    {
+        if (lhs is null)
+        {
+            if (rhs is null)
+            {
+                return true;
+            }
+
+            // Only the left side is null.
+            return false;
+        }
+        // Equals handles case of null on right side.
+        return lhs.Equals(rhs);
+    }
+    public static bool operator != (Complex lhs, Complex rhs)
+    {
+        return !(lhs == rhs);
+    }
+    #endregion
+
 }
--- 02 - 5 mar/02 - 5 mar/Program.cs
+++ 02 - 5 mar/02 - 5 mar/Program.cs
@@ -3,6 +3,27 @@ Console.WriteLine(c1);
 Complex c2 = new Complex(2, -3);
 Console.WriteLine(c2);
 
-Complex c3 = c1.Add(c2);
+//Complex c3 = c1.Add(c2);
+//Console.WriteLine($"{c1} + {c2} = {c3}");
 
-Complex c4 = new Complex(3);
+Complex c4 = c1.Multiply(c2);
+Console.WriteLine($"{c1} * {c2} = {c4}");
+
+Complex c5 = c1 + c2;
+
+Console.WriteLine($"{c1} + {c2} = {c5}");
+
+Complex c6 = new Complex(1, 2);
+Complex c7 = new Complex(1, 2);
+if (c6 == c7)
+    Console.WriteLine("Egale");
+else
+    Console.WriteLine("Nu sunt egale");
+
+
+string s1 = "abc";
+string s2 = "abc";
+if (s1 == s2)
+    Console.WriteLine("Egale");
+else
+    Console.WriteLine("Nu sunt egale");

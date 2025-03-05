40146b65c3fdff59b340d60773fe8698df85d5ef
Horea Oros <horea.oros@gmail.com>
2025-03-05 13:00:20 +0200

    add Complex 5 martie

---
+++ 02 - 5 mar/02 - 5 mar/Complex.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+internal class Complex
+{
+    #region Fields
+    private double re;
+    private double im;
+    #endregion
+
+    #region Constructors
+    public Complex(double re = 0, double im = 0)
+    {
+        this.re = re;
+        this.im = im;
+    }
+
+    //public Complex(): this(0, 0)
+    //{
+        
+    //}
+    #endregion
+
+    #region Methods
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("(");
+        sb.Append(re);
+        sb.Append(", ");
+        sb.Append(im);
+        sb.Append(")");
+        return sb.ToString();
+    }
+
+    public Complex Add(Complex c2)
+    {
+        Complex c = new Complex();
+        c.re = this.re + c2.re;
+        c.im = this.im + c2.im;
+
+        return c;
+    }
+    #endregion
+
+    #region Properties
+    #endregion
+}
---
+++ 02 - 5 mar/02 - 5 mar/Program.cs
@@ -0,0 +1,8 @@
+﻿Complex c1 = new Complex(3, 4);
+Console.WriteLine(c1);
+Complex c2 = new Complex(2, -3);
+Console.WriteLine(c2);
+
+Complex c3 = c1.Add(c2);
+
+Complex c4 = new Complex(3);

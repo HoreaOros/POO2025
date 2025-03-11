e4aa958d96de0f3cc127ab1f854f9707a18de680
Horea Oros <horea.oros@gmail.com>
2025-03-11 12:17:02 +0200

    add functionality to Rational class

--- 03 - 11 mar/03 - 11 mar/Program.cs
+++ 03 - 11 mar/03 - 11 mar/Program.cs
@@ -9,4 +9,33 @@ Console.WriteLine(r3);
 Rational sum = r2.Add(r3);
 Console.WriteLine($"{r2} + {r3} = {sum}");
 
+sum = r2 + r3;
+
+
+List<Rational> lst = new List<Rational>();
+lst.Add(new Rational(3, 4));
+lst.Add(new Rational(1, 2));
+lst.Add(new Rational(3, 8));
+lst.Add(new Rational(5, 6));
+lst.Add(new Rational(1, 4));
+lst.Add(new Rational(7, 8));
+
+lst.Sort();
+
+foreach(var r in lst)
+    Console.WriteLine(r);
+Console.WriteLine();
+
+//lst.Sort(Rational.DescendingSort);
+
+lst.Sort((x, y) => -x.CompareTo(y));
+foreach (var r in lst)
+    Console.WriteLine(r);
+
+
+
+
+
+
+
 
--- 03 - 11 mar/03 - 11 mar/Rational.cs
+++ 03 - 11 mar/03 - 11 mar/Rational.cs
@@ -2,7 +2,7 @@
 
 namespace _03___11_mar
 {
-    public class Rational
+    public class Rational: IEquatable<Rational>, IComparable<Rational>
     {
         #region Fields
         private int numarator, numitor;
@@ -48,12 +48,100 @@ namespace _03___11_mar
         {
             return HashCode.Combine(numarator, numitor);
         }
-        
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Rational);
+        }
+
+        public bool Equals(Rational? p)
+        {
+            if (p is null)
+            {
+                return false;
+            }
+
+            // Optimization for a common success case.
+            if (Object.ReferenceEquals(this, p))
+            {
+                return true;
+            }
+
+            // If run-time types are not exactly the same, return false.
+            if (this.GetType() != p.GetType())
+            {
+                return false;
+            }
+
+            // Return true if the fields match.
+            // Note that the base class is not invoked because it is
+            // System.Object, which defines Equals as reference equality.
+            return this.numarator * p.numitor == this.numitor * p.numarator;
+        }
+
+        public int CompareTo(Rational? other)
+        {
+            // If other is not a valid object reference, this instance is greater.
+            if (other == null) 
+                return 1;
+
+            if (this.Equals(other))
+                return 0;
+            return this.numarator * other.numitor - this.numitor * other.numarator;   
+
+        }
         #endregion
 
         #region Properties
         public int Numarator => numarator;
         public int Numitor => numitor;
         #endregion
+
+        #region Operator overloads
+        public static Rational operator+(Rational lhs, Rational rhs) 
+        {
+            return lhs.Add(rhs);
+        }
+
+        public static bool operator ==(Rational lhs, Rational rhs)
+        {
+            if (lhs is null)
+            {
+                if (rhs is null)
+                {
+                    return true;
+                }
+
+                // Only the left side is null.
+                return false;
+            }
+            // Equals handles case of null on right side.
+            return lhs.Equals(rhs);
+        }
+        public static bool operator <(Rational lhs, Rational rhs)
+        {
+            return lhs.CompareTo(rhs) < 0;  
+        }
+        public static bool operator >(Rational lhs, Rational rhs)
+        {
+            return lhs.CompareTo(rhs) > 0;
+        }
+        public static bool operator <=(Rational lhs, Rational rhs)
+        {
+            return lhs.CompareTo(rhs) <= 0;
+        }
+        public static bool operator >=(Rational lhs, Rational rhs)
+        {
+            return lhs.CompareTo(rhs) >= 0;
+        }
+        public static bool operator !=(Rational lhs, Rational rhs) => !(lhs == rhs);
+        #endregion
+        private class SortDescending : IComparer<Rational>
+        {
+            public int Compare(Rational? x, Rational? y)
+            {
+                return -x.CompareTo(y);
+            }
+        }
+        public static IComparer<Rational> DescendingSort = new SortDescending();
     }
 }

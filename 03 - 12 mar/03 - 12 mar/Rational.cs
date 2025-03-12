df29f3fbb91916a33e7a737c609d02a6b7626377
Horea Oros <horea.oros@gmail.com>
2025-03-12 12:34:25 +0200

    add Rational class skeleton

---
+++ 03 - 12 mar/03 - 12 mar/Rational.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03___12_mar
+{
+    public class Rational
+    {
+        #region Fields
+        private int numarator;
+        private int numitor;
+        #endregion
+
+
+        #region Methods
+        public Rational()
+        {
+            
+        }
+        /// <summary>
+        /// Aduna doua numere rationale
+        /// </summary>
+        public Rational Add(Rational other)
+        {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+
+        #region Properties
+        public int Numarator
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Numitor
+        {
+            get => default;
+            set
+            {
+            }
+        }
+        #endregion
+
+
+    }
+}

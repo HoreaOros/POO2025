0dae9d0fcea89404dec004e24353706e168bea12
Horea Oros <horea.oros@gmail.com>
2025-03-18 12:08:52 +0200

    add 18 mar project (DateTime)

---
+++ 04 - 18 mar/04 - 18 mar/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+// ***********************************************
+// 1.  DateTime - demo
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt.Ticks);
+Console.WriteLine(dt.ToLocalTime());
+Console.WriteLine(dt.ToLongDateString());
+Console.WriteLine(dt.ToOADate());
+Console.WriteLine(dt.ToShortTimeString());
+Console.WriteLine(dt.ToUniversalTime());
+
+DateTimeOffset dt2 = DateTimeOffset.Now;
+Console.WriteLine(dt2);
+
+DateTime dt3 = new DateTime(1254875155584458555, DateTimeKind.Local);
+Console.WriteLine(dt3);
+
+
+DateTime dt4 = new DateTime(5500, 5, 22, new HebrewCalendar());
+Console.WriteLine(dt4);
+
+Console.WriteLine(DateTime.MinValue);
+Console.WriteLine(DateTime.MaxValue);
+
+Console.WriteLine(dt.Year);
+Console.WriteLine(dt.Hour);
+
+Console.WriteLine(dt.DayOfWeek);
+Console.WriteLine(dt.DayOfYear);
+// ***********************************************
+
+
+
+
+// ***********************************************
+// ***********************************************
+// 2. MyDateTime - demo
+MyDateTime md = new MyDateTime(18, 3, 2025, 10, 35, 42);
+Console.WriteLine(md);
+
+MyDateTime md2 = new MyDateTime(23, 6, 2023);
+MyDateTime md3 = new MyDateTime(an: 2025, luna: 12, zi: 13);
+
+Console.WriteLine(md3);
+
+if (md2 == md3)
+{
+    Console.WriteLine($"{md2} / {md3} sunt egale");
+}
+else
+{
+    Console.WriteLine($"{md2} / {md3} nu sunt egale");
+}
+
+Console.WriteLine(md2 - md3); // 
+
+
+// ***********************************************
---
+++ 04 - 18 mar/04 - 18 marTests/MyDateTimeTests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class MyDateTimeTests
+    {
+        [TestMethod()]
+        public void DiffTestSameYear()
+        {
+            // Arrange
+            MyDateTime d1 = new MyDateTime(18, 3, 2025);
+            MyDateTime d2 = new MyDateTime(18, 4, 2025);
+            
+            // Act
+            int expected = 31;
+            
+            // Assert
+            Assert.AreEqual(expected, d2 - d1);
+
+        }
+
+        [TestMethod()]
+        public void DiffTestDifferenYear()
+        {
+            // Arrange
+            MyDateTime d1 = new MyDateTime(18, 3, 2024);
+            MyDateTime d2 = new MyDateTime(18, 3, 2025);
+            int expected = 365;
+
+            // Act
+            int actual = d2 - d1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DiffTestLeapYear()
+        {
+            // Arrange
+            MyDateTime d1 = new MyDateTime(18, 1, 2024);
+            MyDateTime d2 = new MyDateTime(18, 1, 2025);
+            int expected = 366;
+
+            // Act
+            int actual = d2 - d1;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void MyDateTimeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsLeapYearTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EqualsTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CompareToTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CloneTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

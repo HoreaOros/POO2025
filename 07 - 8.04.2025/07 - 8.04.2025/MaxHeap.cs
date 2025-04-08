762980ec35ed6806f3c5bc81fc488b9024b628ea
Horea Oros <horea.oros@gmail.com>
2025-04-08 12:16:50 +0300

    Add PQ functionality to MaxHeap

--- 07 - 8.04.2025/07 - 8.04.2025/MaxHeap.cs
+++ 07 - 8.04.2025/07 - 8.04.2025/MaxHeap.cs
@@ -1,7 +1,16 @@
-﻿public class MaxHeap<T>  where T : IComparable<T>
+﻿using System.Collections;
+
+public class MaxHeap<T>:IEnumerable<T>  where T : IComparable<T>
 {
+    #region Fields
     private T[] data;
+    #endregion
+
+    #region Properties
     public int HeapSize { get; set; }
+    #endregion
+
+    #region Constructors
     public MaxHeap(T[] data)
     {
         this.data = new T[data.Length + 1];
@@ -13,6 +22,9 @@
 
         BuildMaxHeap();
     }
+    #endregion
+
+    #region Methods
     private int Parent(int i)
     {
         return i / 2;
@@ -48,4 +60,74 @@
             MaxHeapify(i);
         }
     }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 1; i <= HeapSize; i++)
+        {
+            yield return data[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return data.GetEnumerator();
+    }
+
+
+    public override string ToString()
+    {
+        return "[" + string.Join(", ", data[1..(HeapSize + 1)]) + "]";
+    }
+    public void HeapSort()
+    {
+        for(int i = HeapSize; i > 1; i--)
+        {
+            (data[1], data[i]) = (data[i], data[1]);
+            HeapSize--;
+            MaxHeapify(1);
+        }
+    }
+
+    public T HeapMaximum()
+    {
+        if (HeapSize < 1)
+            throw new InvalidOperationException("Heap is empty");
+        return data[1];
+    }
+    public T HeapExtractMax()
+    {
+        if (HeapSize < 1)
+            throw new InvalidOperationException("Heap is empty");
+        T max = data[1];
+        data[1] = data[HeapSize];
+        HeapSize--;
+        MaxHeapify(1);
+        return max;
+    }
+    public void HeapIncreaseKey(int i, T key)
+    {
+        if (key.CompareTo(data[i]) < 0)
+            throw new ArgumentException("New key is smaller than current key");
+        data[i] = key;
+        while (i > 1 && data[Parent(i)].CompareTo(data[i]) < 0)
+        {
+            (data[i], data[Parent(i)]) = (data[Parent(i)], data[i]);
+            i = Parent(i);
+        }
+    }
+
+    public void HeapInsert(T key)
+    {
+        HeapSize++;
+
+        if(HeapSize >= data.Length)
+        {
+            Array.Resize(ref data, data.Length * 2);
+        }
+
+        data[HeapSize] = default(T);
+        HeapIncreaseKey(HeapSize, key);
+    }
+    #endregion
 }
--- 07 - 8.04.2025/07 - 8.04.2025/Program.cs
+++ 07 - 8.04.2025/07 - 8.04.2025/Program.cs
@@ -1,4 +1,22 @@
 ﻿int[] data = { 16, 14, 10, 8, 7, 9, 3, 2, 4, 1 };
 int[] data2 = { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
 
-MaxHeap<int> maxHeap = new MaxHeap<int>(data);
+MaxHeap<int> maxHeap = new MaxHeap<int>(data2);
+
+
+
+Console.WriteLine(maxHeap);
+
+
+foreach (var item in maxHeap)
+{
+    Console.WriteLine(item);
+}
+
+//maxHeap.HeapSort();
+//Console.WriteLine("After HeapSort");
+//Console.WriteLine(maxHeap);
+
+maxHeap.HeapInsert(12);
+Console.WriteLine($"After HeapInsert: {maxHeap}");
+

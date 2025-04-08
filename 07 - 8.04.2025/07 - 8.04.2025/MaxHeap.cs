adc742c5b6dca5012f82211cdd2e110fe2fb03ae
Horea Oros <horea.oros@gmail.com>
2025-04-08 11:02:43 +0300

    Add MaxHeap Project

---
+++ 07 - 8.04.2025/07 - 8.04.2025/MaxHeap.cs
@@ -0,0 +1,51 @@
+﻿public class MaxHeap<T>  where T : IComparable<T>
+{
+    private T[] data;
+    public int HeapSize { get; set; }
+    public MaxHeap(T[] data)
+    {
+        this.data = new T[data.Length + 1];
+        for (int i = 0; i < data.Length; i++)
+        {
+            this.data[i + 1] = data[i];
+        }
+        HeapSize = data.Length;
+
+        BuildMaxHeap();
+    }
+    private int Parent(int i)
+    {
+        return i / 2;
+    }
+    private int Left(int i)
+    {
+        return i * 2;
+    }
+    private int Right(int i)
+    {
+        return i * 2 + 1;
+    }
+    private void MaxHeapify(int i)
+    {
+        int left = Left(i);
+        int right = Right(i);
+        int largest = i;
+        if (left <= HeapSize && data[largest].CompareTo(data[left]) < 0)
+            largest = left;
+        if (right <= HeapSize && data[largest].CompareTo(data[right]) < 0)
+            largest = right;
+
+        if(largest != i)
+        {
+            (data[i], data[largest]) = (data[largest], data[i]);
+            MaxHeapify(largest);
+        }
+    }
+    private void BuildMaxHeap()
+    {
+        for (int i = HeapSize / 2; i >= 1; i--)
+        {
+            MaxHeapify(i);
+        }
+    }
+}
---
+++ 07 - 8.04.2025/07 - 8.04.2025/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] data = { 16, 14, 10, 8, 7, 9, 3, 2, 4, 1 };
+int[] data2 = { 4, 1, 3, 2, 16, 9, 10, 14, 8, 7 };
+
+MaxHeap<int> maxHeap = new MaxHeap<int>(data);

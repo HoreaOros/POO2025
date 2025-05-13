96556cf9d19e1c01eed7bb76236977bef166dbfd
Horea Oros <horea.oros@gmail.com>
2025-05-13 10:33:23 +0300

    add new Project (Shapes). add Point class

---
+++ 11 - 13 mai/11 - 13 mai/Point.cs
@@ -0,0 +1,36 @@
+﻿public class Point
+{
+    #region Fields
+    private double x;
+    private double y;
+    #endregion
+
+    #region Constructors
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    #endregion
+
+    #region Properties
+    public double X
+    {
+        get { return x; }
+    }
+    public double Y
+    {
+        get { return y; }
+    }
+    #endregion
+    #region Methods
+    public double DistanceTo(Point other)
+    {
+        return Math.Sqrt(Math.Pow(other.X - this.X, 2) + Math.Pow(other.Y - this.Y, 2));
+    }
+    public override string ToString()
+    {
+        return "[" + X.ToString() + ", " + Y.ToString() + "]";
+    }
+    #endregion
+}
---
+++ 11 - 13 mai/11 - 13 mai/Program.cs
@@ -0,0 +1,4 @@
+﻿Point p = new Point(1, 2);
+Console.WriteLine(p);
+//Rectangle rectangle = new Rectangle(5, 10);
+
